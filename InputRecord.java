48dc01056c61fe414fc243264f187364ec26253e
jarvis0799 <harry2rks@outlook.com>
2025-10-01 12:56:37 -0500

    Create InputRecord.java

---
+++ InputRecord.java
@@ -0,0 +1,10 @@
+public class InputRecord {
+    private String ID;
+    private String CustomField;
+
+    public String getID() { return ID; }
+    public void setID(String ID) { this.ID = ID; }
+
+    public String getCustomField() { return CustomField; }
+    public void setCustomField(String customField) { this.CustomField = customField; }
+}
